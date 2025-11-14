255438047365aefe99bd7283d3f15b0f63875ed7
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2025-11-14 17:18:54 +0100

    Fix headless mode startup with Python 3.11
    Closes #171

--- src/ida_pro_mcp/idalib_server.py
+++ src/ida_pro_mcp/idalib_server.py
@@ -7,6 +7,11 @@ import importlib
 from pathlib import Path
 import typing_inspection.introspection as intro
 
+# Reference: https://errors.pydantic.dev/2.11/u/typed-dict-version
+if sys.version_info < (3, 12):
+    import typing, typing_extensions
+    typing.TypedDict = typing_extensions.TypedDict
+
 from mcp.server.fastmcp import FastMCP
 
 # idapro must go first to initialize idalib
@@ -172,7 +177,7 @@ def main():
     signal.signal(signal.SIGINT, cleanup_and_exit)
     signal.signal(signal.SIGTERM, cleanup_and_exit)
 
-    # NOTE: npx @modelcontextprotocol/inspector for debugging
+    # NOTE: npx -y @modelcontextprotocol/inspector for debugging
     logger.info("MCP Server availabile at: http://%s:%d/sse", mcp.settings.host, mcp.settings.port)
     mcp.run(transport="sse")
 
